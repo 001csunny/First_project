6f449a2bf0f7aca36dd3041e7fda99051d7b91ae
Sunnii <110608081+001csunny@users.noreply.github.com>
2022-11-02 07:04:58 +0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 //set router
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=>{
   res.render('index', { title: 'Express' });
 });
 
-router.get('/events', function(req, res, next) {
+router.get('/events',(req, res, next)=> {
   res.render('events', { title: 'Express' });
 });
 
@@ -22,3 +22,25 @@ router.get('/book', function(req, res, next) {
   res.render('book', { title: 'Express' });
 });
 module.exports = router;
+
+
+//script
+let slide = 1;
+
+showslide=(n)=>{
+  let i;
+  let x = req.body.mySlide
+  if(n > x.length) {
+    slide = 1
+  }else{
+    slide = x.length
+  }
+  for(i = 0;i<x.length; i++){
+    x[i].style.display = "none";
+  }
+  x[slide-1].style.display = "block";
+}
+plus=(n)=>{
+  showslide(slide += n)
+}
+showslide(slide);
